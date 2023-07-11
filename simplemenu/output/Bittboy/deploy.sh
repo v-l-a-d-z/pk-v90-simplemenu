c10014f3167112f7a62f67db6a8179a5560427c6
v-l-a-d-z <>
2023-07-11 22:27:51 +0100

    supress output while deleting macos dot files

--- simplemenu/output/Bittboy/deploy.sh
+++ simplemenu/output/Bittboy/deploy.sh
@@ -4,7 +4,7 @@ echo "Welcome to Taichi's simplemenu deploy script! Initiating..."
 
 # Preparation: clean 'main' partition - delete all macos dot files 
 echo "Deleting all macos dot files"
-find /mnt/ -name '._*' -exec rm -v {} \;
+find /mnt/ -name '._*' -exec rm -v {} &> /dev/null \;
 echo "Preparation completed"
 
 # Step 0: Copy main.sh to a variable for later use
