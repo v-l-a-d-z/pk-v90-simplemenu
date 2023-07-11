9f7ba6b4a6ae0acceb782c214ffdb29d25ec23d2
v-l-a-d-z <>
2023-07-11 22:23:17 +0100

    clean .simplemenu folder on boot

--- simplemenu/output/Bittboy/scripts/autoexec.sh
+++ simplemenu/output/Bittboy/scripts/autoexec.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# If you are planning on connecting the v90's SD Card onto a MacOS device, uncomment the following line:
-#find /mnt/ -name '._*' -exec rm -v {} \;
+find /mnt/.simplemenu -name '._*' -exec rm -v {} \;
 cd /mnt/apps/simplemenu
 ./simplemenu
