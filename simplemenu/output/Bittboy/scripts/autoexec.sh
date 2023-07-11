c23935515171f5325cbdba70dbe2527e908ead89
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2023-07-11 16:24:50 +0000

    Make autoexec cleaner optional

--- simplemenu/output/Bittboy/scripts/autoexec.sh
+++ simplemenu/output/Bittboy/scripts/autoexec.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-find /mnt/ -name '._*' -exec rm -v {} \;
+# If you are planning on connecting the v90's SD Card onto a MacOS device, uncomment the following line:
+#find /mnt/ -name '._*' -exec rm -v {} \;
 cd /mnt/apps/simplemenu
 ./simplemenu
